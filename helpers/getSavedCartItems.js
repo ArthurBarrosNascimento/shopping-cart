9baea43d19832a66d45668d707fe2bf2131b154b
Arthur de Barros <arthurbarros.nascimento12@gmail.com>
2022-07-20 19:31:22 -0300

    requisito sete feito

--- helpers/getSavedCartItems.js
+++ helpers/getSavedCartItems.js
@@ -1,5 +1,8 @@
 const getSavedCartItems = () => {
   // seu código aqui
+  const localSave = localStorage.getItem('carItems');
+
+  return localSave;
 };
 
 if (typeof module !== 'undefined') {
--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -5,5 +5,15 @@ localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  test("Test if, when executing getSavedCartItems, the localStorage.getItem method is called;", async () => {
+    getSavedCartItems();
+
+    expect( await localStorage.getItem).toHaveBeenCalledTimes(1)
+  });
+
+  test("Test if, when executing getSavedCartItems, the localStorage.getItem method is called with 'cartItems' as a parameter", async () => {
+    getSavedCartItems();
+
+    expect( await localStorage.getItem).toHaveBeenCalledWith('carItems');
+  });
 });
